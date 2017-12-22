e4a6728a82e765557b578b86395b6ed094d74970
preecha <preecha@dotography.com>
2017-12-22 12:07:05 +0700

    edit readme.md

--- Readme.md
+++ Readme.md
@@ -1,10 +1,23 @@
-### Modular kit cli
+# Modular kit cli
 ## Feature
-# Create react-native project structure modularity with scale-360-modular-kit
+### Create react-native project structure modularity with scale-360-modular-kit
+
+## How to install
+
+ 
+ 1. clone #develop https://github.com/copygof/modular-cli
+ 2. cd folder modular-cli
+ 3. change branch name in folder config/configModular.js
+ 4. npm link
+ 5. creante temp directory for clone all modular
+ 6. cd temp
+ 7. run ``modular update-version ``
+ 7. select ``All Modular``
+ 7. select ``default from path``
+ 7. select ``y``
 
 
-
-# doctor
+### doctor
 - check import export scene component, keys scene
 
 
@@ -13,3 +26,5 @@
 - remove override android
 - create new feature from modular
 - auto plugin modular to modular
+
+
--- config/configModular.js
+++ config/configModular.js
@@ -8,17 +8,17 @@ const setConfig = (module = []) => {
     // 'https://gitlab.com/copygof/cilnic-student.git#feature/stock',
     // 'https://gitlab.com/copygof/cilnic-student.git#feature/stock',
     
-    "git@bitbucket.org:dotography-code/shopbank-uk-mobile.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/modular-authentication.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/modular-bank.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/galileo.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/modular-marketplace.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/modular-notification.git#feature/update-version-fcm",
-    "git@bitbucket.org:dotography-code/modular-social.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/modular-biometrics.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/auto-kyc-mobile.git#feature/update-version",
+    "git@bitbucket.org:dotography-code/shopbank-uk-mobile.git#integration",
+    "git@bitbucket.org:dotography-code/modular-authentication.git#develop",
+    "git@bitbucket.org:dotography-code/modular-bank.git#develop",
+    "git@bitbucket.org:dotography-code/galileo.git#develop",
+    "git@bitbucket.org:dotography-code/modular-marketplace.git#develop",
+    "git@bitbucket.org:dotography-code/modular-notification.git#develop",
+    "git@bitbucket.org:dotography-code/modular-social.git#develop",
+    "git@bitbucket.org:dotography-code/modular-biometrics.git#develop",
+    "git@bitbucket.org:dotography-code/auto-kyc-mobile.git#develop",
     // "git@bitbucket.org:dotography-code/scale360-native-module.git#feature/update-version",
-    "git@bitbucket.org:dotography-code/animation.git#feature/update-version",
+    "git@bitbucket.org:dotography-code/animation.git#develop",
   ]
   return module.length ? module : configModule
 }
